7840df6979bce1d8dd662f08305e3caeef1bc61c
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 17:00:29 +0530

    ADDED: arrow function to find  full, part and non working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -25,6 +25,7 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
 
 while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ){
     let empCheck = Math.floor(Math.random()*10)%3;
@@ -32,6 +33,7 @@ while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY
     totalEmpHrs += empHrs;
     totalWorkingDays++;
     empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyHrsMap.set(totalWorkingDays,empHrs);
     empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
 }
 
@@ -101,3 +103,26 @@ function totalDaysWorked(numOfDays, dailyWage){
 console.log("UC7G - umber of days Emp worked: "+empDailyWageArr.reduce(totalDaysWorked,0));
 
 
+//UC9
+const findTotal = (totalVal,dailyVal) => {
+    return totalVal+dailyVal;
+}
+
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0)
+                    .reduce(findTotal,0);
+console.log("Using arrow- employee wage = "+" Total hours: "+ totalHours +" Total wage: "+ totalSalary);
+
+let nonWorkingDays = new Array();
+let fullWorkingDays = new Array();
+let partWorkingDays = new Array();
+
+empDailyHrsMap.forEach((value,key,map) => {
+    if(value == 8) fullWorkingDays.push(key);
+    else if(value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full working days: "+fullWorkingDays);
+console.log("Part working days: "+partWorkingDays);
+console.log("Non working days: "+nonWorkingDays);
