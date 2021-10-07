52a3d81293b35fdf219706829596b13f363450d0
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 17:09:47 +0530

    ADDED: object to store employee wage data and showing daily hours worked

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -126,3 +126,27 @@ empDailyHrsMap.forEach((value,key,map) => {
 console.log("Full working days: "+fullWorkingDays);
 console.log("Part working days: "+partWorkingDays);
 console.log("Non working days: "+nonWorkingDays);
+
+//UC10
+{
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArr = new Array();
+    while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyHrsAndWageArr.push(
+            {
+                dayNum:totalWorkingDays,
+                dailyHours:empHrs,
+                dailyWage:calcDailyWage(empHrs),
+                toString(){
+                    return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = ' + this.dailyWage
+                },
+            }
+        );
+    }
+    console.log("UC10 Showing daily hours worked and wage earned: "+empDailyHrsAndWageArr);
+}
